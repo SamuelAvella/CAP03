5e97c06ac5bf654575f3e6538b132889a641d03d
SamuelAvella <saveper1907@g.educaand.es>
2023-10-09 10:29:10 +0200

    Ejericicio

--- TresEjercicio12.java
+++ TresEjercicio12.java
@@ -2,7 +2,12 @@ import java.util.Scanner;
 public class TresEjercicio12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        System.out.print("Introduce la nota del primer examen: ");
+        double primero = Double.parseDouble(System.console().readLine());
+        System.out.print("¿Qué nota quieres sacar en el trimestre? ");
+        double Final = Double.parseDouble(System.console().readLine());
+        double segundo = ((Final * 100) - (primero * 40)) / 60;
+        System.out.printf("Para tener un %.2f en el trimestre necesitas sacar un %d\n en el segundo examen.",Final,(int)Math.round(segundo));
         sc.close();
     }
 }
